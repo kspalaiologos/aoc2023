a3c5add28f36a0dd6ef94aea517beef111fe2686
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-12-07 12:32:00 +0100

    C,hs

---
+++ day7/7.c
@@ -0,0 +1,14 @@
+#include "../a.h"
+typedef struct{C c[5];I b,t;}H;
+I p=1;
+I val(C s){switch(s){case'A':R 14;case'K':R 13;case'Q':R 12;case'J':R p==2?1:11;case'T':R 10;default:R s-'0';}}
+I cmp(H*h1,H*h2){IF(h1->t!=h2->t,R h1->t-h2->t)Fi(5,IF(h1->c[i]!=h2->c[i],R val(h1->c[i])-val(h2->c[i])))}
+I cmpi(I*a,I*b)_(*b-*a)
+void sh(H* h) {
+  I cm[13]={0},jk=0;Fi(5,IF(p==2&&h->c[i]=='J',jk++)ELSE(++cm[val(h->c[i]) - 2]))qsort(cm,13,4,cmpi);
+  switch(cm[0]+=jk){case 1:h->t=0;break;case 2:h->t=cm[1]==2?2:1;break;case 3:h->t=cm[1]==2?4:3;break;default:h->t=cm[0]+1;}}
+I solve(FILE* fp) {
+  H h[1000];I hl=0,ans=0;W(fscanf(fp,"%s %d\n",h[hl].c,&h[hl].b)==2)sh(&h[hl++]);
+  qsort(h,hl,16,cmp);Fi(hl,ans+=h[i].b*(i+1));R ans;}
+I main() {
+  FILE*fp=fopen("input.txt","r");I ans1,ans2;p=1,ans1=solve(fp);rewind(fp);p=2,ans2=solve(fp);printf("%d\n%d\n",ans1,ans2);}
---
+++ day7/7.hs
@@ -0,0 +1,40 @@
+import Data.Char ( digitToInt )
+import Data.List ( group, sortOn, partition, sort, sortBy)
+import qualified Data.Ord
+import Data.Ord (comparing)
+import Control.Monad (liftM2, ap)
+
+readLines :: FilePath -> IO [String]
+readLines file = do
+  contents <- readFile file
+  return $ lines contents
+
+value :: Bool -> Char -> Int
+value _ 'A' = 14
+value _ 'K' = 13
+value _ 'Q' = 12
+value False 'J' = 11
+value True 'J' = 0
+value _ 'T' = 10
+value _ c = digitToInt c
+
+hist :: String -> [(Int, Char)]
+hist = sortBy (comparing Data.Ord.Down) . map (liftM2 (,) length head) . group . sortOn (value True)
+
+classify :: Bool -> String -> [Int]
+classify False hand = map fst (hist hand)
+classify True "JJJJJ" = [5]
+classify True hand = (fst n + sum (map fst jokers)) : map fst rest
+  where (n : rest, jokers) = partition ((/= 'J') . snd) (hist hand)
+
+parse :: String -> (String, Int)
+parse input = (hand, read bid) where [hand, bid] = words input
+
+solve :: Bool -> [(String, Int)] -> Int
+solve = ((sum . zipWith ((`ap` snd) . (. fst) . const . (*)) [1..]) .) . sortOn . (. fst) . ap (ap . ((,) .) . classify) (map . value)
+
+main :: IO ()
+main = do
+  input <- map parse <$> readLines "input.txt"
+  print $ solve False input
+  print $ solve True input
